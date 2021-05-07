2b14aa1e9e5b12f501abaa081a51302ba0eab241
SakJungle <lcjay970324@live.com>
2021-05-07 18:54:21 +0800

    “完成底部导航栏”

---
+++ src/views/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    <NavBar><div slot="center">购物街</div></NavBar>
+
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar'
+  export default {
+    name: "Home",
+   components:{
+      NavBar
+   }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
